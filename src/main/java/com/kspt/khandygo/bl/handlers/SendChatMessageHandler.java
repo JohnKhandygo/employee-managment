c67ab257cfb3d131006fe697d90faa53859677c0
JohnKhandygo <johnkhandygo@gmail.com>
2016-03-09 21:41:10 +0300

    #8 introduce handler for chat message sending use case.

--- src/main/java/com/kspt/khandygo/bl/handlers/SendChatMessageHandler.java
+++ src/main/java/com/kspt/khandygo/bl/handlers/SendChatMessageHandler.java
@@ -1,5 +1,26 @@
 package com.kspt.khandygo.bl.handlers;
 
-public class SendChatMessageHandler {
+import com.kspt.khandygo.bl.messages.ChatMessage;
+import com.kspt.khandygo.core.ExternalStorage;
+import com.kspt.khandygo.core.MessageSender;
+import com.kspt.khandygo.core.entities.Message;
+import com.kspt.khandygo.core.usecases.SendChatMessage;
 
+public class SendChatMessageHandler {
+  private final MessageSender messageSender;
+
+  private final ExternalStorage<Message> messages;
+
+  public SendChatMessageHandler(
+      final MessageSender messageSender,
+      final ExternalStorage<Message> messages) {
+    this.messageSender = messageSender;
+    this.messages = messages;
+  }
+
+  public void handle(final SendChatMessage u) {
+    final ChatMessage m = new ChatMessage(u.origin(), u.author(), u.text());
+    messageSender.send(m);
+    messages.save(m);
+  }
 }
